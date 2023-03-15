0e19eb6337311ca0fd34b197c969ee98283bac8e
dev6jy7 <127913096+dev6jy7@users.noreply.github.com>
2023-03-15 12:09:57 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+console.log("test");
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+
+<p1>test</p1>
+<p1>テスト</p1>
+
+`;
